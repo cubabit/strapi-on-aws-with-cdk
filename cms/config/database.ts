4d04e887c6dbca53467de06a0f709d98c9f212c3
Pete van Holland <pete@ruuby.com>
2024-01-18 22:52:32 +0000

    Change DB config

--- cms/config/database.ts
+++ cms/config/database.ts
@@ -1,22 +1,22 @@
 import path from "node:path";
 
 export default ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '../../', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
   // connection: {
-  //   client: "postgres",
+  //   client: 'sqlite',
   //   connection: {
-  //     host: env("DATABASE_HOST", "127.0.0.1"),
-  //     port: env.int("DATABASE_PORT", 5432),
-  //     database: env("DATABASE_NAME", "strapi"),
-  //     user: env("DATABASE_USERNAME", "admin"),
-  //     password: env("DATABASE_PASSWORD", "admin"),
-  //     ssl: env.bool("DATABASE_SSL", false),
+  //     filename: path.join(__dirname, '../../', env('DATABASE_FILENAME', '.tmp/data.db')),
   //   },
+  //   useNullAsDefault: true,
   // },
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "admin"),
+      password: env("DATABASE_PASSWORD", "admin"),
+      ssl: env.bool("DATABASE_SSL", false),
+    },
+  },
 });
